7295be6113ed9e47f25f0ca2a8c338649c4e3648
Jose <joey_manuel@outlook.com>
2021-02-10 20:50:32 -0500

    6. GameMenu
    Created a GameMenu. Game is ready for merging.

---
+++ Legends_Of_Devslopes/Assets/Scripts/Management Script/GameMenu.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameMenu : MonoBehaviour
+{
+    #region Varaibles
+    [SerializeField]
+    GameObject hero;
+
+    [SerializeField]
+    GameObject tanker;
+
+    [SerializeField]
+    GameObject soldier;
+
+    [SerializeField]
+    GameObject ranger;
+
+    private Animator heroAnim;
+    private Animator tankerAnim;
+    private Animator soldierAnim;
+    private Animator rangerAnim;
+
+    #endregion
+
+
+    #region UnityFunctions
+
+    private void Awake()
+    {
+        Assert.IsNotNull(hero);
+        Assert.IsNotNull(tanker);
+        Assert.IsNotNull(soldier);
+        Assert.IsNotNull(ranger);
+    }
+    // Use this for initialization
+    void Start()
+    {
+        heroAnim = hero.GetComponent<Animator>();
+        tankerAnim = tanker.GetComponent<Animator>();
+        soldierAnim = soldier.GetComponent<Animator>();
+        rangerAnim = ranger.GetComponent<Animator>();
+    }
+
+
+
+    #endregion
+
+    IEnumerator showcase()
+    {
+
+        yield return new WaitForSeconds(1f);
+        heroAnim.Play("SpinAttack");
+        yield return new WaitForSeconds(1f);
+        tankerAnim.Play("Attack");
+        yield return new WaitForSeconds(1f);
+        soldierAnim.Play("Attack");
+        yield return new WaitForSeconds(1f);
+        rangerAnim.Play("Attack");
+        yield return new WaitForSeconds(1f);
+
+        StartCoroutine(showcase());
+    }
+
+    public void Battle()
+    {
+        SceneManager.LoadScene("Level");
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
+
+} // GameMenu class
